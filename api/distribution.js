3728e646f91773ad0bb1e44d0bbdee3ebb3fe62c
JP_Stax <Jacobhenderson.36@gmail.com>
2026-05-17 12:23:49 -0400

    Update distribution.js

--- api/distribution.js
+++ api/distribution.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
   }
-
+ 
   try {
     const r = await fetch(FORGE_URL, {
       headers: {
